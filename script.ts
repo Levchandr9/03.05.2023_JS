4033e238bfdf50eb31b76f6f35129b86a7ce4ae1
Levchandr9 <levon.vagarshakovich9@gmail.com>
2023-05-05 00:34:22 +0300

    ДЗ 05.03

---
+++ script.ts
@@ -0,0 +1,44 @@
+
+5.
+// let positive = 0
+// let negative = 0
+// let zero = 0
+// let even = 0
+// let odd = 0
+
+// for (let i = 0; i < 5; i++ ){
+//     let m = +prompt('введите число')
+//     if  (m == 0){
+//         zero++
+//     }
+//     else if (m % 2 == 0){
+//         even++
+//         if (m > 0){
+//             positive++
+//         }
+//         else if (m < 0){
+//             negative++
+//         }
+//     }
+//     else if (m % 2 != 0){
+//         odd++
+//         if (m > 0){
+//             positive++
+//         }
+//         else if (m < 0){
+//             negative++
+//         }
+//     }
+//     else{
+//         alert('произошло что-то непридвиденное')
+//     }
+// }
+
+// alert(
+//     `Ты ввел 
+//     количество положительных цифр = ${positive}
+//     количество отрицательных цифр = ${negative}
+//     количество нулей = ${zero} 
+//     количество четных цифр = ${even} 
+//     количество нечетных = ${odd}`
+// )
